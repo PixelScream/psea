e6465d0ccea40bf4d185291035f05ef4e12bee73
PixelScream <underscoredickie@gmail.com>
2014-10-26 19:48:47 +0000

    code formatting and scrolling anchor points

--- js/main.js
+++ js/main.js
@@ -3,9 +3,19 @@ window.onload = function(){
 
   var renders = document.getElementsByClassName("render");
   var w = screen.width * 0.8;
+  var menuOut = false;
 
-  
-
+  var menuButton =  document.getElementById('menu-button');
+  menuButton.onclick = function() {
+    console.log('clicked');
+    if (menuOut) {
+      document.getElementById('menu-container').style.maxHeight = "";
+    }
+    else {
+      document.getElementById('menu-container').style.maxHeight = "500px";
+    }
+    menuOut = !menuOut;
+  }
 /*
   for (var i = 0; i < renders.length; i++) {
     //alert(i);
@@ -29,8 +39,102 @@ window.onload = function(){
 
   }
   */
-}
+  var scrollTopOffset = 0;
+  var links = document.getElementsByTagName("a");
+  for ( var i = 0; i < links.length; i++) {
+    ref = "" + links[i].getAttribute("href");
+    if( ref.charAt(0) == "#") {
+      var r = links[i];
+      //console.log(ref.slice(1) + " is an anchor, scrolls to :" + newY);
+      r.onclick = function() {
+        console.log( this);
+        
+        var newY = document.getElementById(this.getAttribute("href").slice(1)).offsetTop;
+        console.log("scrolling to " + newY);
+        TweenLite.to(window, 2, {scrollTo:{y:(newY - scrollTopOffset)}, ease:Power2.easeOut});
+        if (document.getElementById('menu-container').style.maxHeight != 0 ) {
+          document.getElementById('menu-container').style.maxHeight = "";
+          menuOut = false;
+        } 
+        return false;
+      }
+    }
+  }
 
+  var scrollBuffer = 5
+  var logo = document.getElementById("logo");
+  var nav = document.getElementById("nav-container");
+  window.onscroll = scroll;
+  var downClass = 'down';
+  var hidClass = 'hid';
+
+
+  var yStor = window.pageYOffset;
+  var lastDown = yStor;
+  var lastUp = yStor;
+
+  function scroll () {
+    var y = window.pageYOffset;
+    var navClass = nav.className.split(" ");
+    var logoClass = logo.className.split(" ");
+    // nav tweening
+    if (y > scrollBuffer) {
+      if ( checkForClass(navClass, downClass) == false ) {
+        nav.className += " " + downClass;
+      }
+      if (checkForClass(logoClass, downClass) == false) {
+        logo.className = downClass;
+      }
+    }
+    else {
+      if ( checkForClass(navClass, downClass) == true ) {
+        nav.className = nav.className.replace( /(?:^|\s)down(?!\S)/ , "");
+      }
+      if ( checkForClass(logoClass, downClass) == true) {
+        logo.className = "";
+      }
+    }
+
+    var curMargin = nav.style.marginTop;
+    if (curMargin == "") {
+      curMargin = 0;
+    }
+    //console.log("lastUp :" + lastUp + ", lastDown :" + lastDown + ", y :" + y, ", deltas :" + (lastDown - y) + " " + (lastUp - y));
+    //console.log(parseInt(curMargin) + " " + (-nav.offsetHeight));
+    
+    if (yStor - y < 0) {
+      //console.log("going down");
+      if (y > 200) {
+        if ( checkForClass(navClass, hidClass) == false ) {
+          nav.className += " " + hidClass;
+        }
+      }
+      if (document.getElementById('menu-container').style.maxHeight != 0 ) {
+        document.getElementById('menu-container').style.maxHeight = "";
+        menuOut = false;
+      } 
+    }
+    else
+    {
+      //console.log("coming up");
+      if ( checkForClass(navClass, hidClass) == true ) {
+        nav.className = nav.className.replace( /(?:^|\s)hid(?!\S)/ , "");
+      }
+    }
+    yStor = y;
+    
+  }
+
+}
+  function checkForClass(classArray, lookingFor) {
+    //console.log("checking " + classArray.length + ", " + classArray + " & " + lookingFor);
+    if (classArray != undefined) {
+      for (var i = 0; i <= classArray.length; i++) {
+        if (classArray[i] == lookingFor) { return true; }
+      }
+    }
+    return false;
+  }
 /*
   var logo = document.getElementById("logo");
 
@@ -48,73 +152,3 @@ window.onload = function(){
   window.onscroll = scrollFunction();
 
 */
-var scrollBuffer = 5
-var logo = document.getElementById("logo");
-var nav = document.getElementById("nav-container");
-window.onscroll = scroll;
-var downClass = 'down';
-var hidClass = 'hid';
-
-
-var yStor = window.pageYOffset;
-var lastDown = yStor;
-var lastUp = yStor;
-
-function scroll () {
-  var y = window.pageYOffset;
-  var navClass = nav.className.split(" ");
-  var logoClass = logo.className.split(" ");
-  // nav tweening
-  if (y > scrollBuffer) {
-    console.log("checking nav");
-    if ( checkForClass(navClass, downClass) == false ) {
-      nav.className += " " + downClass;
-    }
-    console.log("checking logo");
-    if (checkForClass(logoClass, downClass) == false) {
-      logo.className = downClass;
-    }
-  }
-  else {
-    if ( checkForClass(navClass, downClass) == true ) {
-      nav.className = nav.className.replace( /(?:^|\s)down(?!\S)/ , "");
-    }
-    if ( checkForClass(logoClass, downClass) == true) {
-      logo.className = "";
-    }
-  }
-
-  var curMargin = nav.style.marginTop;
-  if (curMargin == "") {
-    curMargin = 0;
-  }
-  //console.log("lastUp :" + lastUp + ", lastDown :" + lastDown + ", y :" + y, ", deltas :" + (lastDown - y) + " " + (lastUp - y));
-  //console.log(parseInt(curMargin) + " " + (-nav.offsetHeight));
-  
-  if (yStor - y < 0) {
-    console.log("going down");
-    if (y > 200) {
-      if ( checkForClass(navClass, hidClass) == false ) {
-        nav.className += " " + hidClass;
-      }
-    }
-  }
-  else
-  {
-    console.log("coming up");
-    if ( checkForClass(navClass, hidClass) == true ) {
-      nav.className = nav.className.replace( /(?:^|\s)hid(?!\S)/ , "");
-    }
-  }
-  yStor = y;
-  
-}
-function checkForClass(classArray, lookingFor) {
-  console.log("checking " + classArray.length + ", " + classArray + " & " + lookingFor);
-  if (classArray != undefined) {
-    for (var i = 0; i <= classArray.length; i++) {
-      if (classArray[i] == lookingFor) { return true; }
-    }
-  }
-  return false;
-}
